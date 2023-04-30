ba6b1648609a9fe4422b30761011588ce8d2e1d3
nimanoro <nimanourozy@gmail.com>
2023-04-29 23:52:00 -0700

    added data loader

--- dataloader.py
+++ dataloader.py
@@ -7,32 +7,47 @@ from torchvision import transforms
 from PIL import Image
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, Subset
-def loadtotensor(dir, type, number):
 
-    # Set the path to your dataset folder
-    data_path = "data/data_simple/"
-
+def loadtotensor(dir):
     # Define the number of images per folder
     num_images_per_folder = 10
 
     # Create the dataset with all the images
-    dataset = ImageFolder(root=data_path)
+    dataset = ImageFolder(root=dir)
 
     # Create a list of indices for each folder to select only the desired number of images
     folder_indices = []
-    for folder_name in os.listdir(data_path):
-        folder_path = os.path.join(data_path, folder_name)
-        folder_images = os.listdir(folder_path)
-        folder_indices.extend([dataset.index((os.path.join(folder_path, image_name))) for image_name in folder_images[:num_images_per_folder]])
+    for folder_name in os.listdir(dir):
+        if folder_name != ".DS_Store":
+            folder_path = os.path.join(dir, folder_name + "/")
+            folder_images = [f for f in os.listdir(folder_path) if f.endswith('.jpg')]
+            if folder_images:
+                folder_indices.extend([dataset.samples.index((os.path.join(folder_path, image_name), dataset.class_to_idx[folder_name])) for image_name in folder_images[:num_images_per_folder]])
 
     # Create a subset of the dataset with only the desired images
+    transform = transforms.Compose([transforms.ToTensor()])
     subset = Subset(dataset, folder_indices)
-
+    for i in subset:
+        subset.append(transform.totensor(i))
     # Create a DataLoader object with a batch size of 32
     batch_size = 32
     dataloader = DataLoader(subset, batch_size=batch_size, shuffle=True)
-    print (len(dataloader))
-    # Iterate over the data in batches
-    #for data in dataloader:
-        
-    # Do something with the data
+
+    return dataloader
+
+
+data_loader = loadtotensor("data/data_simple/")
+
+# Get a random batch
+batch = next(iter(data_loader))
+
+# Get a random image from the batch
+#for i in batch:
+    #image = batch[i]
+    #transform = transforms.ToTensor()
+    #tensor_image = transform(image)
+        #convert to PIL image
+    #pil_image = transforms.ToPILImage()(tensor_image)
+    # Display the image using matplotlib
+    #plt.imshow(image)
+    #plt.show()
